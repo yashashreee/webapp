97abc5c18a702eb4488b13ae181bcc2a06a7fe0e
YashashreePatel <yashashreepatel@gmail.com>
2024-02-07 19:46:28 -0500

    adding user APIs/assignment2

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../configs/database');
+
+const Users = sequelize.define('Users', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  account_created: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  account_updated: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+}, {
+  timestamps: false,
+});
+
+module.exports = Users;
