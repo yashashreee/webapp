72fe99488e7d377115949a11b1727609edd520df
YashashreePatel <yashashreepatel@gmail.com>
2024-02-13 16:25:00 -0500

    updating chages after assignment-2 review

--- src/middlewares/user-middleware.js
+++ src/middlewares/user-middleware.js
@@ -1,11 +1,12 @@
 const bcrypt = require('bcrypt');
 const Users = require('../models/user');
+const responseHeaders = require('../headers');
 
 const basicAuth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Basic ')) {
-    return res.status(401).json({ error: 'Unauthenticated! Please enter credentials' });
+    return res.status(401).header(responseHeaders).json({ error: 'Unauthenticated! Please enter credentials' });
   }
 
   const base64Credentials = authHeader.split(' ')[1];
@@ -16,18 +17,18 @@ const basicAuth = async (req, res, next) => {
     const user = await Users.findOne({ where: { email } });
 
     if(!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).header(responseHeaders).json({ error: 'User not found' });
     }
     
     if (user && await bcrypt.compare(password, user.password)) {
       req.user = user;
       next();
     } else {
-      return res.status(401).json({ error: 'Unauthenticated! Please enter valid credentials' });
+      return res.status(401).header(responseHeaders).json({ error: 'Unauthenticated! Please enter valid credentials' });
     }
   } catch (error) {
     console.error(error);
-    return res.status(503).json({ error: 'Service unavailable' });
+    return res.status(503).header(responseHeaders).json({ error: 'Service unavailable' });
   }
 };
 
