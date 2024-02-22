e0c72e8494122e3ff99e4f6663b46039aa30f6ab
YashashreePatel <yashashreepatel@gmail.com>
2024-02-22 15:10:39 -0500

    bug fix: updated db.js

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize({
-  port: process.env.PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASS, {
   dialect: process.env.DIALECT,
-  host: process.env.HOST,
+  host: process.env.HOST
 });
 
 const syncDatabase = async () => {
