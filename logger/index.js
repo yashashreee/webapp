5eb1271e49389c3a8e764c94515be9d0cdf0614f
YashashreePatel <yashashreepatel@gmail.com>
2024-03-19 16:23:35 -0400

    adding logger file

---
+++ logger/index.js
@@ -0,0 +1,30 @@
+const { LoggingWinston } = require('@google-cloud/logging-winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, json } = format;
+const appRoot = require("app-root-path");
+
+const loggingWinston = new LoggingWinston();
+
+const logger = createLogger({
+  transports: [
+    loggingWinston,
+    new transports.File({
+      filename: `${appRoot}/logs/webapp.log`,
+      level: 'info',
+      format: combine(
+        timestamp(),
+        json()
+      )
+    }),
+
+    new transports.Console({
+      format: combine(
+        timestamp(),
+        json(),
+        format.prettyPrint()
+      )
+    })
+  ]
+});
+
+module.exports = logger;
