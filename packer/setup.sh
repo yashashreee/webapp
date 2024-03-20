ea3367bb98ba58fccbf396e34d8207741575b89e
YashashreePatel <yashashreepatel@gmail.com>
2024-03-20 15:59:08 -0400

    fix:permission errors

--- packer/setup.sh
+++ packer/setup.sh
@@ -13,7 +13,7 @@ echo "-------------------------------------------------------------------------"
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-sudo cp -f tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+sudo cp -f /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 
 
 echo "-------------------------------------------------------------------------"
