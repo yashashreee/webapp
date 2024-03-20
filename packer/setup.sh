3e0686025a24ceff23f1c2b05f6a424b0f29ab7d
YashashreePatel <yashashreepatel@gmail.com>
2024-03-19 23:05:45 -0400

    updating setup filr for packer

--- packer/setup.sh
+++ packer/setup.sh
@@ -7,7 +7,7 @@ curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 sudo systemctl start google-cloud-ops-agent
 
-sudo mv tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
+sudo cp tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 
 
 echo "-------------------------------------------------------------------------"
