2187c1691c522948677dbcd7f26fa77f189a881a
YashashreePatel <yashashreepatel@gmail.com>
2024-03-20 14:24:23 -0400

    chaining var folder permission

--- packer/setup.sh
+++ packer/setup.sh
@@ -3,6 +3,7 @@
 echo "-------------------------------------------------------------------------"
 echo "Installing Ops Agent"
 echo "-------------------------------------------------------------------------"
+sudo chmod -R 777 /var
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
